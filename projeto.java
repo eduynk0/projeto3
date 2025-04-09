6e9abde5093daecf33e290d6e56a8082079ebf21
eduynk0 <eduynk0@gmail.com>
2025-04-09 08:56:43 -0300

    Update projeto.java

--- projeto.java
+++ projeto.java
@@ -1,1 +1,22 @@
-
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner eduardo = new Scanner(System.in);
+        
+        System.out.print("Número entre 1 a 100 para ser adivinhado: ");
+		int al = eduardo.nextInt();
+		
+		int tentativas = 1;
+		int  palpite = 0;
+		
+		while (palpite != al) {
+		    System.out.print("Número entre 1 a 100: ");
+		    palpite = eduardo.nextInt();
+		    
+			if (palpite > al ) {
+				System.out.println("Menor");
+				tentativas += 1;}
+			if (palpite < al ) {
+				System.out.println("Mais");
+				tentativas += 1;}}
+        System.out.print(al + " é a resposta, " + tentativas + " tentativas");}}
